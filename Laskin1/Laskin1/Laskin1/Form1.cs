5ffc635ef7a47595b16bec95c337906887fedfd3
jezzze <jesse.mitronen@gmail.com>
2021-05-21 19:15:19 +0300

    versio1

--- Laskin1/Laskin1/Laskin1/Form1.cs
+++ Laskin1/Laskin1/Laskin1/Form1.cs
@@ -10,11 +10,69 @@ using System.Windows.Forms;
 
 namespace Laskin1
 {
-    public partial class Form1 : Form
+    public partial class equation : Form
     {
-        public Form1()
+        Double value = 0;
+        string operaatio = "";
+        bool operaatio_painettu = false;
+        public equation()
         {
             InitializeComponent();
+        }
+
+      
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if ((tulos.Text == "0")||(operaatio_painettu))
+                tulos.Clear();
+
+            operaatio_painettu = false;
+            Button b = (Button)sender;
+            tulos.Text = tulos.Text + b.Text;
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            tulos.Text = "0";
+        }
+
+        private void operaattori_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            operaatio = b.Text;
+            value = Double.Parse(tulos.Text);
+            operaatio_painettu = true;
+            yhtalo.Text = value + " " + operaatio;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            yhtalo.Text = "";
+            switch (operaatio)
+            {
+                case "+":
+                    tulos.Text = (value + Double.Parse(tulos.Text)).ToString();
+                    break;
+                case "-":
+                    tulos.Text = (value - Double.Parse(tulos.Text)).ToString();
+                    break;
+                case "*":
+                    tulos.Text = (value * Double.Parse(tulos.Text)).ToString();
+                    break;
+                case "/":
+                    tulos.Text = (value / Double.Parse(tulos.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }//switch loppu
+            
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            tulos.Text = "";
+            value = 0;
         }
     }
 }
--- Laskin1/Laskin1/Laskin1/Program.cs
+++ Laskin1/Laskin1/Laskin1/Program.cs
@@ -16,7 +16,7 @@ namespace Laskin1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new equation());
         }
     }
 }
